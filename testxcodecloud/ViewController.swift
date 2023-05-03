41db5011ae8570b249e6ddf7047b9cb7f50c4136
eduardm <eduardm@adobe.com>
2023-05-03 13:53:33 +0400

    Initial Commit

---
+++ testxcodecloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testxcodecloud
+//
+//  Created by Eduard Minasyan on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
